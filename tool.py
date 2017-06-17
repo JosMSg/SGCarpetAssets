adfcbf5e7d5a7a5df78c5e97fb286d3920152790
Jos <jos@gmail.com>
2017-06-17 00:58:45 -0500

    Get files

--- tool.py
+++ tool.py
@@ -22,6 +22,16 @@ if user_type in sg_types:
 			index_name = int(raw_input("Opcion seleccionada:"))
 			user_name = dir_names[index_name-1]
 			print "Usted selecciono: %s" %user_name
+		else: 
+			"Erroneous data"
+		dir_files = os.listdir(os.getcwd()+'/'+user_category+'/'+user_name)
+		if len(dir_files)>0:
+			print "Seleccione un nombre:"
+			for index, name in enumerate(dir_files):
+				print '%s - %s' %(index+1, name)
+			index_file = int(raw_input("Opcion seleccionada:"))
+			user_file = dir_files[index_name-1]
+			print "Usted selecciono: %s" %user_file
 else:
 	print 'Revise que este en la carpeta correcta y el nombre de la misma sea el adecuado'
 raw_input("Try again")
